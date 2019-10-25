b3f771be133f1b3df2dbe7d3f62be20b63402a3b
Melina <melinamonserrat2010@hotmail.com>
2019-10-25 14:01:53 -0600

    what

--- app/src/main/java/com/example/infinitescroling/EditProfileActivity.java
+++ app/src/main/java/com/example/infinitescroling/EditProfileActivity.java
@@ -3,40 +3,15 @@ package com.example.infinitescroling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
-
-import com.example.infinitescroling.adapters.EditAcademicAdapter;
-import com.example.infinitescroling.models.AcademicInfo;
-
-import java.util.ArrayList;
-import java.util.Date;
+import android.util.Log;
+import android.view.View;
 
 public class EditProfileActivity extends AppCompatActivity {
-
-    private ListView academicListView;
-    private ArrayList<AcademicInfo> academics;
-    private EditAcademicAdapter adapter;
-    private Spinner genderSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
-
-        academicListView = findViewById(R.id.ListView_academics);
-        academics = new ArrayList<>();
-        //TODO populate with firestorm info instead
-        academics.add(new AcademicInfo("Bachillerato", "Lic. Alfaro ruiz", new Date(2013, 2, 15), new Date()));
-        adapter = new EditAcademicAdapter(this, academics);
-        academicListView.setAdapter(adapter);
-
-        genderSpinner = findViewById(R.id.spinner_genderSelection);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.strArr_genders, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        genderSpinner.setAdapter(adapter);
 
         //TODO fill form spaces with the actual information of the user
         //TODO listing academic information and editing it
--- app/src/main/java/com/example/infinitescroling/LoginActivity.java
+++ app/src/main/java/com/example/infinitescroling/LoginActivity.java
@@ -1,38 +1,19 @@
 package com.example.infinitescroling;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private int CREAR_CUENTA = 1;
-    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-
-    private EditText emailInput;
-    private EditText passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        if(firebaseAuth.getCurrentUser() != null){
-            //TODO go to main activity
-        }
-
-        emailInput = findViewById(R.id.editText_emailInput);
-        passwordInput = findViewById(R.id.editText_passwordInput);
     }
 
 
@@ -40,33 +21,5 @@ public class LoginActivity extends AppCompatActivity {
         //TODO crear metodo para recibir el resultado del activity
         Intent registerIntent = new Intent(this, RegisterActivity.class);
         startActivityForResult(registerIntent, CREAR_CUENTA);
-    }
-
-    public void loginOnClick(View view){
-        //TODO make this an actual login
-
-        String email = emailInput.getText().toString();
-        String password = passwordInput.getText().toString();
-
-        if(email.equals("") || password.equals("")){
-            Toast.makeText(this, R.string.str_loginIncomplete,Toast.LENGTH_LONG).show();
-            return;
-        }
-        firebaseAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-            @Override
-            public void onSuccess(AuthResult authResult) {
-                gotoMainActivity();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(LoginActivity.this, R.string.str_loginFailed, Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    private void gotoMainActivity(){
-        Intent intent =  new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
 }
