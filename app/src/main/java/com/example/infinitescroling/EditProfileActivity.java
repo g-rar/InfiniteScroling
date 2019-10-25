eeee53756589c7a127ca04a8134312fb49bc2385
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2019-10-25 13:47:23 -0600

    Added login functionality and user model.

--- app/src/main/java/com/example/infinitescroling/EditProfileActivity.java
+++ app/src/main/java/com/example/infinitescroling/EditProfileActivity.java
@@ -3,7 +3,9 @@ package com.example.infinitescroling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.example.infinitescroling.adapters.EditAcademicAdapter;
 import com.example.infinitescroling.models.AcademicInfo;
@@ -16,6 +18,7 @@ public class EditProfileActivity extends AppCompatActivity {
     private ListView academicListView;
     private ArrayList<AcademicInfo> academics;
     private EditAcademicAdapter adapter;
+    private Spinner genderSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,12 @@ public class EditProfileActivity extends AppCompatActivity {
         academics.add(new AcademicInfo("Bachillerato", "Lic. Alfaro ruiz", new Date(2013, 2, 15), new Date()));
         adapter = new EditAcademicAdapter(this, academics);
         academicListView.setAdapter(adapter);
+
+        genderSpinner = findViewById(R.id.spinner_genderSelection);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.strArr_genders, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(adapter);
 
         //TODO fill form spaces with the actual information of the user
         //TODO listing academic information and editing it
