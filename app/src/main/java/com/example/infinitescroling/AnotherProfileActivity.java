1aab649d1eedd8fe1b05ebf32a773b5a5f2e3b29
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2019-10-29 09:11:06 -0600

    Half Done changes

--- app/src/main/java/com/example/infinitescroling/AnotherProfileActivity.java
+++ app/src/main/java/com/example/infinitescroling/AnotherProfileActivity.java
@@ -6,8 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AnotherProfileActivity extends AppCompatActivity {
 
+    private String profileUserId;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_another_profile);
+
+
+
+        profileUserId = getIntent().getStringExtra("userId");
+
     }
 }
